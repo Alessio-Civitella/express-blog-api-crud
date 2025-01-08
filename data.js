e1537fb1b9c9eef8da1fea0f9f230c174ae310c4
alessio-civitella <alessio.civitella19@gmail.com>
2025-01-08 17:20:26 +0100

    Feat: aggiunto cors al file

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 const posts = [
     {
-      titolo: "L'arte del coding",
-      contenuto: "Scopri come migliorare le tue competenze di programmazione con consigli e best practice.",
-      immagine: "/ciambellone.jpeg",
+      title: "L'arte del coding",
+      content: "Scopri come migliorare le tue competenze di programmazione con consigli e best practice.",
+      image: "/ciambellone.jpeg",
       tags: ["coding", "programmazione", "sviluppo web"],
       id:1
     },
     {
-      titolo: "Viaggio tra le stelle",
-      contenuto: "Un'esplorazione delle più affascinanti scoperte astronomiche recenti.",
-      immagine: "/cracker_barbabietola.jpeg",
+      title: "Viaggio tra le stelle",
+      content: "Un'esplorazione delle più affascinanti scoperte astronomiche recenti.",
+      image: "/cracker_barbabietola.jpeg",
       tags: ["astronomia", "scienza", "universo"],
       id:2
     },
     {
-      titolo: "Cucina gourmet a casa tua",
-      contenuto: "Ricette semplici e veloci per portare la cucina stellata nella tua cucina.",
-      immagine: "/pane_fritto_dolce.jpeg",
+      title: "Cucina gourmet a casa tua",
+      content: "Ricette semplici e veloci per portare la cucina stellata nella tua cucina.",
+      image: "/pane_fritto_dolce.jpeg",
       tags: ["cucina", "gourmet", "ricette"],
       id:3
     },
     {
-      titolo: "Fitness: allenarsi a casa",
-      contenuto: "Ecco una guida su come mantenerti in forma senza uscire di casa.",
-      immagine: "/pasta_barbabietola.jpeg",
+      title: "Fitness: allenarsi a casa",
+      content: "Ecco una guida su come mantenerti in forma senza uscire di casa.",
+      image: "/pasta_barbabietola.jpeg",
       tags: ["fitness", "salute", "benessere"],
       id:4
     },
     {
-      titolo: "I migliori libri del 2024",
-      contenuto: "Una selezione dei libri imperdibili di quest'anno, da leggere assolutamente.",
-      immagine: "/torta_paesana.jpeg",
+      title: "I migliori libri del 2024",
+      content: "Una selezione dei libri imperdibili di quest'anno, da leggere assolutamente.",
+      image: "/torta_paesana.jpeg",
       tags: ["libri", "lettura", "2024"],
       id:5
     }
